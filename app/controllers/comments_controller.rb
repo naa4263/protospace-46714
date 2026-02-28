fa12c3d1ae76506885ef84eed5543f6d8bc9157c
naa4263 <naa42638@gmail.com>
2026-02-28 21:06:20 +0900

    protospace修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,11 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to root_path
+      redirect_to prototype_path(@comment.prototype)
     else
-      render :new,status: unprocessable_entity
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show",status: :unprocessable_entity
     end
   end
 
